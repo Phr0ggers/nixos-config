9cd8bdfa768c7137bb70769b1059e067554bad80
sebastianf <sebastian_fohringer@icloud.com>
2025-07-17 12:48:07 +0200

    initial commit

---
+++ hosts/nixos/configuration.nix
@@ -0,0 +1,127 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = false;
+	boot.loader.systemd-boot.consoleMode = "max";
+	boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "us";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # media-session.enable = true;
+  };
+
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.sebastianf = {
+    isNormalUser = true;
+    description = "sebastianf";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim 
+    gcc
+    neofetch
+		discord-ptb
+    # wget
+    # sbctl niv # do not delete!    
+  ];
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+  ];
+
+  home-manager = {
+    # also pass inputs to home-manager modules
+    extraSpecialArgs = {inherit inputs;};
+    users = {
+      "sebastianf" = import ./home.nix;
+    };
+  };
+
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # services.openssh.enable = true;
+
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
---
+++ hosts/nixos/home.nix
@@ -0,0 +1,52 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "sebastianf";
+  home.homeDirectory = "/home/sebastianf";
+
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+
+  home.packages = [
+  ];
+
+  home.file = {
+  };
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  programs.home-manager.enable = true;
+
+
+  programs.neovim = {
+    enable = true;
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "sebastianf";
+    userEmail = "sebastian_fohringer@icloud.com";
+
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
+
+  programs.kitty = {
+    enable = true;
+    
+    themeFile = "GruvboxMaterialDarkMedium";
+
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      size = 12;
+    };
+    settings = {
+      enable_audio_bell = "no";
+      window_padding_width = 5;
+      confirm_os_window_close = 0;
+    };
+  };
+
+}
