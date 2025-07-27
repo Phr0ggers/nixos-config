af2df9e807a9648dfc7bc242803828e826a77410
sebastianf <sebastian_fohringer@icloud.com>
2025-07-27 21:02:52 +0200

    wlogout.nix

--- modules/home-manager/wlogout.nix
+++ modules/home-manager/wlogout.nix
@@ -6,6 +6,35 @@
   config = lib.mkIf config.wlogout.enable {
     programs.wlogout = {
       enable = true;
+      layout = [
+        {
+          label = "shutdown";
+          action = "poweroff";
+          text = "Shutdown";
+          keybind = "s";
+        }
+        {
+          label = "reboot";
+          action = "reboot";
+          text = "Reboot";
+          keybind = "r";
+        }
+        {
+          label = "logout";
+          action = "loginctl terminate-user $USER";
+          text = "Logout";
+          keybind = "l";
+        }
+        {
+          label = "windows";
+          action = "";
+          text = "Windows";
+          keybind = "w";
+        }
+      ];
+      style = ''
+
+      '';
     };
   };
 }
